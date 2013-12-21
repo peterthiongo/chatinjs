2a0bf8569efbfb7f72849cb37e251f33edd84a94
peterthiongo <peterg44@yahoo.com>
2013-12-21 09:25:22 +0000

    Chat change guess q

--- simple-express.js
+++ simple-express.js
@@ -9,8 +9,7 @@ app.configure( function() {
 
 // Routes
 app.get('/', function(req, res) {
-    res.send('Hello orld');
+    res.send('Hello world, guess what this is?');
 });
-
 
 app.listen(3000);
